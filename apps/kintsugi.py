cb540dca266dfc9e9462eebeba8c4247e20a7681
shreyaa2104 <shreyaaprajapati2004@gmail.com>
2026-07-22 21:53:24 +0530

    Update logo

--- apps/kintsugi.py
+++ apps/kintsugi.py
@@ -77,7 +77,7 @@ col1, col2 = st.columns([1, 8])
 
 with col1:
     st.markdown("<div style='margin-top:12px'></div>", unsafe_allow_html=True)
-    st.image("klogo.png", width=90)
+    st.image("KS.png", width=90)
 
 with col2:
     st.title("KINTSUGI")
